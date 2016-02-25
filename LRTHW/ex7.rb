5b32983d11171e8ce7b2f7e65f7320cedfda839f
Bernard Pietraga <bernard.pietraga@gmail.com>
2016-02-25 16:48:54 +0100

    Exercise 7 added.

---
+++ LRTHW/ex7.rb
@@ -0,0 +1,36 @@
+#coding -*- utf-8 -*-
+
+# Bernard Pietraga
+# Excercise No. 7 from Learn Ruby The Hard Way
+
+# Prints string about Mary
+puts "Mary had a little lamb."
+
+# Prints string about Mars's lamb fleece color.
+# W ith "Snow" text inside Variable added to string
+puts "Its fleece was white as #{'snow'}."
+
+# Prints string about Mary
+puts "And everywhere that Mary went."
+
+# Multiplies dot character.
+puts "." * 10 # What would that do?
+
+# Declaring variavbles with only single values.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print vs puts. Comparsion of printing metods.
+# Because od "print" everything appears on one line.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12

5d5c2ef4fcbeb33687826a35145d7eb477ef0a5d
Bernard Pietraga <bernard.pietraga@gmail.com>
2016-03-23 11:03:02 +0100

    Exercise 24, 25, 26 added.
    All working, on purpose errors in exercise 26 were fixed as written in
    the book.

---
+++ LRTHW/ex24.rb
@@ -0,0 +1,51 @@
+#coding -*- utf-8 -*-
+
+# Bernard Pietraga
+# Excercise No. 24 from Learn Ruby The Hard Way
+
+
+puts "Let's practice everything."
+puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t ta'
+
+# the <<END is a "heredoc".
+poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires and explantaion
+\n\t\twhere there is none.
+END
+
+# Prints a string.
+puts "______________"
+# Prints a variable poem with string inside.
+puts poem
+# Prints a string.
+puts "______________"
+
+# Maths.
+five = 10 - 2 + 3 - 6
+# Prints a string with varible five executed.
+puts "This should be five: #{five}"
+
+# Secret formula.
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+
+# Defines variable.
+start_point = 10000
+# Gets return values from formula.
+# Runs secret_formula function with start_point value set for 10000.
+beans, jars, crates = secret_formula(start_point)
+
+# Prints a string with start_point variable value.
+puts "With a starting point of: #{start_point}"
+# Prints a string with return values of called secret_formula.
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} cartes."
+# Divides start_point variable by 10.
+start_point /= 10
---
+++ LRTHW/ex25.rb
@@ -0,0 +1,57 @@
+#coding -*- utf-8 -*-
+
+# Excercise No. 25 from Learn Ruby The Hard Way
+
+# Module can be run from irb.
+# Below text to be pasted into irb to run the script.
+# require "./ex25.rb"
+# To use functions declare variable called sentence with a string inside.
+# Example line.
+# words = Ex25.break_words(sentence)
+
+module Ex25
+
+  # This function will break up words for us.
+  def Ex25.break_words(stuff)
+    words = stuff.split(' ')
+    return words
+  end
+
+  # Sorts the words.
+  def Ex25.sort_words(words)
+    return words.sort
+  end
+
+  # Prints the first word after shifting it off.
+  def Ex25.print_first_word(words)
+    word = words.shift
+    puts word
+  end
+
+  # Prints the last word after poppin it off.
+  def Ex25.print_last_word(words)
+    word = words.pop
+    puts word
+  end
+
+  # Takes in a full sentence and returns the sorted words.
+  def Ex25.sort_sentence(sentence)
+    words = Ex25.break_words(sentence)
+    return Ex25.sort_words(words)
+  end
+
+  # Prints the first and last words of sentence.
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+  # Sorts the words then prints the first and the last one.
+  def Ex25.print_first_and_last_sorted(sentence)
+    words = Ex25.sort_sentence(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+end

8624e6edb0f99858fd834157f844e9ec111fc75b
Bernard Pietraga <bernard.pietraga@gmail.com>
2016-05-02 12:53:15 +0200

    Indentation style fix in test_ex47.rb
    Fix to get proper indentation in test_ex47.rb.

--- LRTHW/ex47/tests/test_ex47.rb
+++ LRTHW/ex47/tests/test_ex47.rb
@@ -42,41 +42,41 @@ class TestGame < Test::Unit::TestCase
 
   # Study drill
   def test_ex45game
-    # Creates room obejcts.
-    ground = Room.new("Ground", "You wake up on ground.")
-    park = Room.new("Park", "You look around.")
-    taxi = Room.new("Taxi", "You ask driver and go with taxi.")
-    hotel = Room.new("hotel", "You see hotel boy.")
-    booking_stand = Room.new("BookingStand", "You speak with hotel boy.")
-    finish = Room.new("Finish", "You  didn't understand what happened.")
+      # Creates room obejcts.
+      ground = Room.new("Ground", "You wake up on ground.")
+      park = Room.new("Park", "You look around.")
+      taxi = Room.new("Taxi", "You ask driver and go with taxi.")
+      hotel = Room.new("hotel", "You see hotel boy.")
+      booking_stand = Room.new("BookingStand", "You speak with hotel boy.")
+      finish = Room.new("Finish", "You  didn't understand what happened.")
 
-    # Shows possible game paths.
-    # From ground player can go to park and ground.
-    ground.add_paths({'park'=> park, 'ground'=> ground})
-    # From park player can go to park, taxi and hotel.
-    park.add_paths({'park'=> park,  'taxi'=> taxi, 'hotel' => hotel})
-    # From taxi player can go to taxi and hotel.
-    taxi.add_paths({'taxi'=> taxi, 'hotel'=> hotel})
-    # From hotel player can go to hotel and booking_stand.
-    hotel.add_paths({'hotel'=> hotel, 'booking_stand'=> booking_stand})
-    # From booking_stand player can go to booking_stand and finish.
-    booking_stand.add_paths({'booking_stand'=> booking_stand, 'finish'=> finish})
+      # Shows possible game paths.
+      # From ground player can go to park and ground.
+      ground.add_paths({'park'=> park, 'ground'=> ground})
+      # From park player can go to park, taxi and hotel.
+      park.add_paths({'park'=> park,  'taxi'=> taxi, 'hotel' => hotel})
+      # From taxi player can go to taxi and hotel.
+      taxi.add_paths({'taxi'=> taxi, 'hotel'=> hotel})
+      # From hotel player can go to hotel and booking_stand.
+      hotel.add_paths({'hotel'=> hotel, 'booking_stand'=> booking_stand})
+      # From booking_stand player can go to booking_stand and finish.
+      booking_stand.add_paths({'booking_stand'=> booking_stand, 'finish'=> finish})
 
-    # Tests the route from ground to park to taxi
-    # to hotel to booking stand to finish.
-    assert_equal(finish, ground
-                        .go('park')
-                        .go('taxi')
-                        .go('hotel')
-                        .go('booking_stand')
-                        .go('finish'))
-    # Tests the routes from ground to park
-    # to hotel to booking stand to finish.
-    assert_equal(finish, ground
-                        .go('park')
-                        .go('hotel')
-                        .go('booking_stand')
-                        .go('finish'))
+      # Tests the route from ground to park to taxi
+      # to hotel to booking stand to finish.
+      assert_equal(finish, ground
+                          .go('park')
+                          .go('taxi')
+                          .go('hotel')
+                          .go('booking_stand')
+                          .go('finish'))
+      # Tests the routes from ground to park
+      # to hotel to booking stand to finish.
+      assert_equal(finish, ground
+                          .go('park')
+                          .go('hotel')
+                          .go('booking_stand')
+                          .go('finish'))
   end
 
 end

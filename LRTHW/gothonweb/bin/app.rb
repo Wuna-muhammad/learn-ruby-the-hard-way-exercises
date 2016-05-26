9761c1041e964386c21435a56020e4d8a660db11
Bernard Pietraga <bernard.pietraga@gmail.com>
2016-05-26 11:46:09 +0200

    Exercise 50 added.
    Not so fancy webpage on ruby sinatra framework.

---
+++ LRTHW/gothonweb/bin/app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+set :port, 8080
+set :static, true
+set :public_folder, "static"
+set :views, "views"
+
+get '/' do
+  return 'Hello world'
+end
+
+get '/hello/' do
+  greeting = params[:greeting] || "Hi there"
+  erb :index, :locals => {'greeting' => greeting}
+end
+
+# Redirects url to html file.
+get '/howdy' do
+  redirect "./howdy.html"
+end
+
+# Dynamilcy generates content.
+# The friend is parameter passed to sinatra.
+# Framework generates uniqe page for every argument.
+# Ie. http://localhost:8080/hi/Bernard
+get '/hi/:friend' do
+  return "Hi #{params['friend']}, you are sinatra friend."
+end
---
+++ LRTHW/gothonweb/tests/test_gothonweb.rb
@@ -0,0 +1,10 @@
+require "./bin/app.rb"
+require "test/unit"
+
+class TestGOTHONWEB < Test::Unit::TestCase
+
+  def test_sample
+    assert_equal(4, 2+2)
+  end
+
+end
